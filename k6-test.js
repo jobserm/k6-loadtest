93a10e1032252e33a34ed379be8abedede65a4d2
Jinna Chodchoy <jinna.chodchoy@agoda.com>
2021-11-08 23:38:49 +0700

    add load test

---
+++ k6-test.js
@@ -0,0 +1,64 @@
+import http from "k6/http";
+import { check, group, sleep } from "k6";
+import { Rate } from "k6/metrics";
+
+// A custom metric to track failure rates
+var failureRate = new Rate("check_failure_rate");
+
+
+const FE_URL = process.env.FE_URL
+const BE_URL = process.env.BE_URL
+
+// Options
+export let options = {
+    stages: [
+        // Linearly ramp up from 1 to 50 VUs during first minute
+        { target: 50, duration: "1m" },
+        // Hold at 50 VUs for the next 3 minutes and 30 seconds
+        { target: 50, duration: "2m" },
+        // Linearly ramp down from 50 to 0 VUs over the last 30 seconds
+        { target: 0, duration: "30s" }
+        // Total execution time will be ~5 minutes
+    ],
+    thresholds: {
+        // We want the 95th percentile of all HTTP request durations to be less than 500ms
+        "http_req_duration": ["p(95)<800"],
+        // Requests with the staticAsset tag should finish even faster
+        "http_req_duration{staticAsset:yes}": ["p(95)<250"],
+        // Thresholds based on the custom metric we defined and use to track application failures
+        "check_failure_rate": [
+            // Global failure rate should be less than 1%
+            "rate<0.01",
+            // Abort the test early if it climbs over 5%
+            { threshold: "rate<=0.05", abortOnFail: true },
+        ],
+    },
+};
+
+// Main function
+export default function () {
+    let response = http.get(BE_URL);
+
+    // check() returns false if any of the specified conditions fail
+    let checkRes = check(response, {
+        "status is 200": (r) => r.status === 200,
+        "content is present": (r) => r.body.indexOf("JobSerm, backend dev. team!") !== -1,
+    });
+
+    // We reverse the check() result since we want to count the failures
+    failureRate.add(!checkRes);
+
+    // Load static assets, all requests
+    group("Static Assets", function () {
+        // Execute multiple requests in parallel like a browser, to fetch some static resources
+        let resps = http.batch([
+            ["GET", FE_URL, null, { tags: { staticAsset: "yes" } }],
+        ]);
+        // Combine check() call with failure tracking
+        failureRate.add(!check(resps, {
+            "status is 200": (r) => r[0].status === 200,
+        }));
+    });
+
+    sleep(Math.random() * 3 + 2); // Random sleep between 2s and 5s
+}
